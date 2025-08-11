821538d4914130b1982a9f9bd07939061f7700e8
StageDog <aksanajw845@gmail.com>
2025-08-11 12:05:07 +0800

    feat: 检测最低酒馆助手版本

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,7 +2,11 @@ import { register_lorebook } from '@client/lorebook';
 import { register_preset } from '@client/preset';
 import { compare } from 'compare-versions';
 
-$(() => {
+$(async () => {
+  const version = await getTavernHelperVersion();
+  if (compare(version, '3.4.0', '<')) {
+    throw new Error('酒馆助手版本过低, 不能使用酒馆同步脚本, 请更新至 3.4.0 或更高版本');
+  }
   register_lorebook();
   register_preset();
 });
