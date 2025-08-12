32bdb09a25ed44e0a8c03753546d8090f9dc54f1
StageDog <aksanajw845@gmail.com>
2025-08-12 16:17:31 +0800

    feat: 启动酒馆助手版本检测

--- src/client/index.ts
+++ src/client/index.ts
@@ -5,7 +5,7 @@ import { compare } from 'compare-versions';
 $(async () => {
   const version = await getTavernHelperVersion();
   if (compare(version, '3.4.0', '<')) {
-    //   throw new Error('酒馆助手版本过低, 不能使用酒馆同步脚本, 请更新至 3.4.0 或更高版本');
+    throw new Error('酒馆助手版本过低, 不能使用酒馆同步脚本, 请更新至 3.4.0 或更高版本');
   }
   register_worldbook();
   register_preset();
