e3615bedce3a4857aaf68d68028b35abe6a15f60
StageDog <aksanajw845@gmail.com>
2025-08-13 21:51:53 +0800

    fix: 使用 websocket 而非 polling 以改进脚本性能

--- src/client/socket.ts
+++ src/client/socket.ts
@@ -6,17 +6,15 @@ let socket: Socket | null = null;
 export function get_socket(): Socket {
   if (!socket) {
     const settings = get_settings();
-    socket = io(settings.url, { retries: 3 });
+    socket = io(settings.url, {
+      transports: ['websocket'],
+    });
 
     socket.on('connect', () => {
       console.info('[TavernSync] 成功连接至服务器');
     });
     socket.on('connect_error', (error: Error) => {
-      if (socket!.active) {
-        console.warn(`[TavernSync] 连接服务器出错, 尝试重连! ${error.stack}`);
-      } else {
-        console.error(`[TavernSync] 连接服务器出错, 请手动连接重试! ${error.stack}`);
-      }
+      console.error(`[TavernSync] 连接服务器出错, 尝试重连! ${error.stack}`);
     });
     socket.on('disconnect', (reason, details) => {
       console.info(`[TavernSync] 与服务器断开连接: ${reason}\n${JSON.stringify(details)}`);
