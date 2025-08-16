1f961ddc2a0cf401521fa2ae6b4f3bdbf109f554
StageDog <aksanajw845@gmail.com>
2025-08-16 19:21:06 +0800

    fix: 捕获配置不存在错误

--- src/server/component/argument.ts
+++ src/server/component/argument.ts
@@ -1,6 +1,7 @@
 import { beauingfy_configs } from '@server/component/beauingfy_configs';
 import { get_settings } from '@server/settings';
 import { create_syncer } from '@server/syncer/factory';
+import { exit_on_error } from '@server/util/exit_on_error';
 
 import { Argument, Command } from 'commander';
 
@@ -9,7 +10,7 @@ export function add_configs_to_command(command: Command): Command {
   command.addArgument(
     new Argument('<config>', '配置名称').choices(Object.keys(settings.configs)).argParser(value => {
       if (!(value in settings.configs)) {
-        throw Error(`配置 '${value}' 不存在, ${beauingfy_configs()}`);
+        exit_on_error(`配置 '${value}' 不存在, ${beauingfy_configs()}`);
       }
       return create_syncer(value, settings.configs[value]);
     }),
