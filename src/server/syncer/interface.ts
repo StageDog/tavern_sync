18116e97805d61e3a2b5f8f9e8386b88a03f55eb
StageDog <aksanajw845@gmail.com>
2025-08-16 17:47:48 +0800

    fix: 避免 sanitize 完整路径

--- src/server/syncer/interface.ts
+++ src/server/syncer/interface.ts
@@ -2,7 +2,6 @@ import { is_collection_file } from '@server/component/collection_file';
 import { watch_on } from '@server/component/watch_on';
 import { close_server, wait_socket } from '@server/server';
 import { exit_on_error } from '@server/util/exit_on_error';
-import { sanitize_filename } from '@server/util/sanitize_filename';
 import { translate } from '@server/util/translate';
 import { write_file_recursively } from '@server/util/write_file_recursively';
 import _ from 'lodash';
@@ -56,7 +55,7 @@ export abstract class Syncer_interface {
     this.type_zh = type_zh;
     this.config_name = config_name;
     this.name = name;
-    this.file = resolve(__dirname, sanitize_filename(file));
+    this.file = resolve(__dirname, file);
     this.dir = dirname(this.file);
 
     this.en_type = en_type;
