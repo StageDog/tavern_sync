487ae08c00fbe7d06f4729703b7c65722c3b020e
StageDog <aksanajw845@gmail.com>
2025-08-19 11:01:36 +0800

    feat: 监听所有配置

--- src/server/command/watch.ts
+++ src/server/command/watch.ts
@@ -1,20 +1,35 @@
-import { add_configs_to_command } from '@server/component/argument';
-import { Syncer_interface } from '@server/syncer/interface';
+import { beauingfy_configs } from '@server/component/beauingfy_configs';
+import { get_settings } from '@server/settings';
+import { create_syncer } from '@server/syncer/factory';
+import { exit_on_error } from '@server/util/exit_on_error';
 
-import { Command } from 'commander';
+import { Argument, Command } from 'commander';
 
 export function add_watch_command(): Command {
   const command = new Command('watch').description('监听本地内容的变化并实时推送到酒馆');
 
-  add_configs_to_command(command);
+  const settings = get_settings();
+  command.addArgument(
+    new Argument('[config]', '配置名称, 不填则监听所有配置').choices(Object.keys(settings.configs)).argParser(value => {
+      if (!(value in settings.configs)) {
+        exit_on_error(`配置 '${value}' 不存在, ${beauingfy_configs()}`);
+      }
+    }),
+  );
   command.option(
     '-f, --force',
     '强制推送: 如果本地文件中的条目名称或数量与酒馆中的不一致, 将会覆盖酒馆中的内容',
     false,
   );
 
-  command.action(async (syncer: Syncer_interface, options: { force: boolean }) => {
-    await syncer.watch({ should_force: options.force });
+  command.action(async (config: string, options: { force: boolean }) => {
+    if (config) {
+      await create_syncer(config, settings.configs[config]).watch({ should_force: options.force });
+    } else {
+      for (const config of Object.keys(settings.configs)) {
+        await create_syncer(config, settings.configs[config]).watch({ should_force: options.force });
+      }
+    }
   });
   return command;
 }
