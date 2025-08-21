56ac7d1a5ca5cbc17fa5ae3ebd569d75ed496b77
StageDog <aksanajw845@gmail.com>
2025-08-21 21:44:51 +0800

    fix: 修复合集文件的名称匹配

--- src/server/component/collection_file.ts
+++ src/server/component/collection_file.ts
@@ -7,8 +7,26 @@ interface Collection_file_entry {
   content: string;
 }
 export function parse_collection_file(content: string): Collection_file_entry[] {
-  return [...content.matchAll(/ *\# \^([^\n]+)\n([\s\S]*?)(?:(?=\n *\# \^[^\n]+\n)|$)/gs)].map(match => ({
-    name: match[1],
-    content: match[2],
-  }));
+  const lines = content.split('\n');
+  const entry_anchors = _(lines)
+    .map((line, index): { name?: string; line_number: number } | null => {
+      const match = line.match(/^ *\# \^(.*)/);
+      if (match) {
+        return { name: match[1].trimEnd(), line_number: index };
+      }
+      return null;
+    })
+    .filter(value => value !== null)
+    .push({ name: undefined, line_number: lines.length })
+    .value();
+
+  return _(entry_anchors.slice(0, -1))
+    .zip(entry_anchors.slice(1))
+    .map(([entry_anchor, next_entry_anchor]) => {
+      return {
+        name: entry_anchor!.name!,
+        content: lines.slice(entry_anchor!.line_number + 1, next_entry_anchor!.line_number).join('\n'),
+      };
+    })
+    .value();
 }
