5a92668454180378c3265c8fc8edca116d23f185
StageDog <aksanajw845@gmail.com>
2025-08-29 19:05:47 +0800

    fix: is_parent 返回值

--- src/server/util/is_parent.ts
+++ src/server/util/is_parent.ts
@@ -2,5 +2,5 @@ import { isAbsolute, relative } from 'path';
 
 export function is_parent(parent_path: string, possible_child_path: string) {
   const result = relative(parent_path, possible_child_path);
-  return result && !result.startsWith('..') && !isAbsolute(result);
+  return Boolean(result) && !result.startsWith('..') && !isAbsolute(result);
 }
