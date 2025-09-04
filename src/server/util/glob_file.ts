b0ba1482c66d6f8c11bbcfa5a309b3996cdb398b
StageDog <aksanajw845@gmail.com>
2025-09-04 09:58:13 +0800

    fix: escape glob expression

--- src/server/util/glob_file.ts
+++ src/server/util/glob_file.ts
@@ -2,5 +2,5 @@ import { globSync } from 'node:fs';
 import { resolve } from 'node:path';
 
 export function glob_file(base: string, file: string): string[] {
-  return globSync(resolve(base, file) + '{.*,}');
+  return globSync(resolve(base, file).replaceAll(/[\[\]\{\}]/g, '[$&]') + '{.*,}');
 }
