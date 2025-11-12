237b5a3efd6da851ca7bd8fe2cc148d91ed22069
StageDog <aksanajw845@gmail.com>
2025-11-12 12:05:02 +0800

    feat: 将 user_name 作为可选

--- src/server/component/replace_user_name.ts
+++ src/server/component/replace_user_name.ts
@@ -1,5 +1,9 @@
 import { get_settings } from '@server/settings';
 
 export function replace_user_name(text: string | undefined): string | undefined {
-  return text?.replaceAll(get_settings().user_name, '<user>');
+  const user_name = get_settings().user_name;
+  if (user_name) {
+    return text?.replaceAll(user_name, '<user>');
+  }
+  return text;
 }
