df61ca06d258cfc4d879a37499de57e16b91e57e
StageDog <aksanajw845@gmail.com>
2026-02-13 17:21:00 +0800

    feat: 保证打包时换行符是 LF 风格

--- src/server/util/extract_file_content.ts
+++ src/server/util/extract_file_content.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from 'node:fs';
 
 export function extract_file_content(path: string): string {
-  return readFileSync(path, 'utf-8');
+  return readFileSync(path, 'utf-8').replaceAll('\r\n', '\n');
 }
